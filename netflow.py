44cbe359edcc6cd743d1e31b2e04d54b7ac261a1
Connor Lange <lange.connor90@gmail.com>
2012-02-26 23:21:58 -0800

    Fleshed out the database storage, still isn't persistent (cPickle?)

--- netflow.py
+++ netflow.py
@@ -2,13 +2,24 @@ from socket import socket as sock, AF_INET, SOCK_DGRAM
 from socket import ntohl, ntohs
 from select import select
 from struct import unpack
-from time import ctime
+import time
 
 # Configuration Parameters 
 DATA_READ = 1024
 POLL = 0
 HDR_LEN = 24
 DATA_LEN = 48
+
+# HEADER INDEX CONSTANTS
+VERSION = 0
+NUM_FLOWS = 1
+UPTIME = 2
+EPOCH_MS = 3
+EPOCH_NS = 4
+TOTAL_FLOWS = 5
+ENGINE_TYPE = 6
+ENGINE_ID = 7
+SAMPLE_RATE = 8
 
 # RECORD INDEX CONSTANTS
 SRC_IP = 0
@@ -126,21 +137,21 @@ def formatIP(ipAddr):
   oct2 = (ipAddr & 0x00FF0000) >> 16
   oct3 = (ipAddr & 0x0000FF00) >> 8
   oct4 = (ipAddr & 0x000000FF)
-#  print "IP: "+str(oct1)+"."+str(oct2)+"."+str(oct3)+"."+str(oct4)
   return str(oct1)+"."+str(oct2)+"."+str(oct3)+"."+str(oct4)
 
 class Collector:
-  def __init__(self, host="", port=9996):
+  def __init__(self, db=Database(), host="", port=9996):
     self.listener = sock(AF_INET, SOCK_DGRAM, 0)
     self.listener.bind((host, port))
     self.buffer = ""
+    self.database = db
     self.pktLogBin = open("netflowlogBin.txt", "wb")
     self.pktLog = open("netflowlog.txt", "w")
   
   # "Main" Method to collect and store data
   def collectNetFlowPackets(self):
     if self.getData():
-      print "Data!"
+      print "Flow Received!"
       self.parseNetFlowPackets() 
    
   # Close the socket 
@@ -161,16 +172,16 @@ class Collector:
 
   # Parse the NetFlow V5 header and any associated flows
   def parseNetFlowPackets(self):
-    numFlows = self.parseNetFlowHeader(self.buffer[:HDR_LEN])
-    print numFlows
+    hdrData = self.parseNetFlowHeader(self.buffer[:HDR_LEN])
     idx = HDR_LEN
+    numFlows = hdrData[NUM_FLOWS]
     while numFlows > 0:
       self.parseNetFlowRecord(self.buffer[idx:idx+DATA_LEN])
       numFlows -= 1
       idx += DATA_LEN
 
   # Header Format (24 bytes)
-  # 0                         16                32  bits
+  # 0                         16                 32 bits
   # |    NetFlow Version      |     Flow Count    | 
   # |              Device Uptime                  | 
   # |          milliseconds since Epoch           |
@@ -179,11 +190,24 @@ class Collector:
   # | engine type | engine ID | sampling interval | 
   # @param pktData packet data as a string 
   def parseNetFlowHeader(self, pktData):
-    print repr(pktData)
-    pktPayload = unpack("!hhllllbbh", pktData)
-    print pktPayload
-    return pktPayLoad
+    pktPayload = list(unpack("!HHLLLLBBH", pktData))
+    self.dumpHeader(pktPayload)
+    self.storeHeader(pktPayload, time.time())
+    return pktPayload
 
+  # DEBUG (DUMP HEADER)
+  def dumpHeader(self, pktData):
+    print "==== NetFlow Header Dump ===="
+    print "NetFlow Version: "+str(pktData[VERSION])
+    print "Number of Flows in this Export: "+str(pktData[NUM_FLOWS])
+    print "System Uptime in Milliseconds: "+str(pktData[UPTIME])
+    print "Time since Epoch in Milliseconds: "+str(pktData[EPOCH_MS])
+    print "Residual Nanoseconds from Above: "+str(pktData[EPOCH_NS])
+    print "Total Number of Flows Since Boot: "+str(pktData[TOTAL_FLOWS])
+    print "Engine Type: "+str(pktData[ENGINE_TYPE])
+    print "Engine ID: "+str(pktData[ENGINE_ID])
+    print "Sampling Interval: "+str(pktData[SAMPLE_RATE])
+  
   # DEBUG (DUMP RECORD)
   def dumpRecord(self, pktData):
     srcIP = formatIP(pktData[SRC_IP])
@@ -230,52 +254,71 @@ class Collector:
   # http://netflow.caligare.com/netflow_v5.htm
   def parseNetFlowRecord(self, pktData):
     pktPayload = list(unpack("!LLLHHLLLLHHBBBBHHBBH", pktData))
-    print pktPayload
-    
     self.dumpRecord(pktPayload) 
-    #totalL3bytes = ntohl(int(pktData[20:24]))
-    #startOfFlow = ntohl(int(pktData[24:28]))
-    #lastFlowTime = ntohl(int(pktData[28:32]))
-    #tcpUdpSrc = ntohs(int(pktData[32:34]))
-    #tcpUdpDst = ntohs(int(pktData[34:36]))
-    #tcpFlagsSumm = int(pktData[37:38])
-    #ipProtocolType = int(pktData[38:39])
-    #tos = int(pktData[39:40])
-    #srcAS = ntohs(int(pktData[40:42]))
-    #dstAS = ntohs(int(pktData[42:44]))
-    #srcMask = int(pktData[44:45])
-    #dstMask = int(pktData[45:46])
+    self.storeRecord(pktPayload[:2], pktPayload[2:])
     return pktPayload
-    #recordKey = (srcIP, dstIP)
 
-    #return (recordKey, pktPayload)
-    
+  # Stores flows by src and dest
+  def storeRecord(self, key, record):
+    self.db.insert("R", key, record)
+  
+  # Stores headers by order received 
+  def storeHeader(self, key, header):  
+    self.db.insert("H", key, record)
+
 class Database:
   def __init__(self):
-    database = { }
+    self.data = {"R": {}, "H":{}}
+    self.numHdrs = 0
+  
+  def insert(self, table, key=None, data):
+    if table == "H":
+      self.data[table][self.numHdrs] = HeaderInfo(data)
+      self.numHdrs += 1
+    elif table == "R":
+      try:   
+        self.data[table][key].update(key, data)
+      except KeyError:
+        self.data[table][key] = RecordInfo(data)
 
-class DatabaseRecord:
-  def __init__(self, nextHop, snmpInput, snmpOutput, numPkts, totalL3bytes, \
-   startOfFlow, endOfFlow, tcpUdpSrc, tcpUdpDst, tcpFlagsSum, ipProtType, tos, \
-    srcAs, dstAs, srcMask, dstMask):
-    
-    self.nextHop = nextHop
-    self.snmpIn = snmpInput
-    self.snmpOut = snmpOutput
-    self.numPkts = numPkts
-    self.L3Bytes = totalL3bytes 
-    self.flowStart = startOfFlow
-    self.flowStop = endOfFlow
-    self.srcPort = tcpUdpSrc
-    self.dstPort = tcpUdpDst
-    self.tcpFlags = tcpFlagsSum
-    self.ipProt = ipProtType
-    self.tos = tos
-    self.srcAS = srcAs
-    self.dstAS = dstAs 
+class NetFlowInfo:
+  def __init__(self, timestampMs):
+    pass
+
+class HeaderInfo(NetFlowInfo):
+  def __init__(self, dataGroup):    
+    self.version = dataGroup[VERSION]
+    self.numFlows = dataGroup[NUM_FLOWS]
+    self.uptime = dataGroup[UPTIME]
+    self.epochMs = dataGroup[EPOCH_MS]
+    self.epochNs = dataGroup[EPOCH_NS]
+    self.totalFlows = dataGroup[TOTAL_FLOWS]
+    self.engineType = dataGroup[ENGINE_TYPE]
+    self.engineID = dataGroup[ENGINE_ID]  
+    self.samplingInterval = dataGroup[SAMPLE_RATE]
+
+class RecordInfo(NetFlowInfo):
+  def __init__(self, dataGroup):  
+    self.nextHop = dataGroup[HOP_IP-2]
+    self.snmpIn = dataGroup[IF_IN-2]
+    self.snmpOut = dataGroup[IF_OUT-2]
+    self.numPkts = dataGroup[NUM_PKTS-2]
+    self.L3Bytes = dataGroup[L3_BYTES-2]
+    self.flowStart = dataGroup[START-2]
+    self.flowStop = dataGroup[END-2]
+    self.srcPort = dataGroup[SRC_PORT-2]
+    self.dstPort = dataGroup[DST_PORT-2]
+    self.tcpFlags = dataGroup[TCP_FLAGS-2]
+    self.ipProt = dataGroup[IP_PROT-2]
+    self.tos = dataGroup[SRV_TYPE-2]
+    self.srcAS = dataGroup[SRC_AS-2]
+    self.dstAS = dataGroup[DST_AS-2] 
+    self.srcMask = dataGroup[SRC_MASK-2]
+    self.dstMask = dataGroup[DST_MASK-2]
+
+  def update(self, key, dataGroup):
+    print "Got another record for: "+str(key) 
    
-    
-
 if __name__ == "__main__":
   flowCollector = Collector()
   data = Database()
@@ -283,6 +326,6 @@ if __name__ == "__main__":
   while True:
     flowCollector.collectNetFlowPackets()
  # except Exception: 
-  #  print "Error!\nWriting raw packets to log!"
+  # "Error!\nWriting raw packets to log!"
    # flowCollector.pktLog.close()
    # exit()
