8422e26c650984cca2e242545d448e5893b43622
Chris Brenton <chrisb@ifixit.com>
2012-03-03 19:57:41 -0800

    Lots of visualization stuff: canvas sized correctly and scales appropriately, basic GL drawing, switched to ortho2D, added update function.

--- visualize.py
+++ visualize.py
@@ -2,12 +2,14 @@
 
 import wx
 import sys
+import random
 
 from wx import glcanvas
 
 # The Python OpenGL package can be found at
 # http://PyOpenGL.sourceforge.net/
 from OpenGL.GL import *
+from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 """
@@ -18,12 +20,18 @@ as well as:
 """
 class MyCanvasBase(glcanvas.GLCanvas):
     def __init__(self, parent):
-        glcanvas.GLCanvas.__init__(self, parent, -1)
+        #glcanvas.GLCanvas.__init__(self, parent, -1, size=(100,300))
+        glcanvas.GLCanvas.__init__(self, parent, -1, size=(100, 150))
         self.init = False
         # initial mouse position
         self.lastx = self.x = 30
         self.lasty = self.y = 30
         self.size = None
+
+        self.r = 1.0
+        self.g = 0.0
+        self.b = 0.0
+
         self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
         self.Bind(wx.EVT_SIZE, self.OnSize)
         self.Bind(wx.EVT_PAINT, self.OnPaint)
@@ -32,15 +40,28 @@ class MyCanvasBase(glcanvas.GLCanvas):
         self.Bind(wx.EVT_MOTION, self.OnMouseMotion)
         self.Bind(wx.EVT_KEY_DOWN, self.onKeyPress)
 
+    def drawColor(self):
+        glColor3f(self.r, self.g, self.b)
+        print "glColor3f(%d, %d, %d)" % (self.r, self.g, self.b)
+
     def OnEraseBackground(self, event):
         pass # Do nothing, to avoid flashing on MSW.
 
     def OnSize(self, event):
-        minDimension = min(self.GetGLExtents())
+        #minDimension = min(self.GetGLExtents())
+        (w, h) = self.GetGLExtents()
         if self.GetContext():
             self.SetCurrent()
-            glViewport(0, 0, minDimension, minDimension)
+            #glViewport(0, 0, minDimension, minDimension)
+            #glViewport(0, 0, (int)(size[0] * 0.7), size[1])
+            glViewport(0, 0, w, h)
         event.Skip()
+
+    def update(self):
+        self.r = random.random()
+        self.g = random.random()
+        self.b = random.random()
+        self.OnDraw()
 
     def OnPaint(self, event):
         dc = wx.PaintDC(self)
@@ -78,6 +99,8 @@ class MyCanvasBase(glcanvas.GLCanvas):
         elif chr(keycode) == 'Q':
             print "quitting"
             app.Exit()
+        elif chr(keycode) == 'R':
+            self.update()
         else:
             print chr(keycode)
         event.Skip()
@@ -86,18 +109,7 @@ class MyCanvasBase(glcanvas.GLCanvas):
 
     def OnInitGL(self):
         """Initialize OpenGL for use in the window."""
-        glClearColor(1, 1, 1, 1)
-
-    def OnReshape(self, width, height):
-        """Reshape the OpenGL viewport based on the dimensions of the window."""
-        glViewport(0, 0, width, height)
-
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(-1.0, 1.0, -1.0, 1.0, 1.0, 3.0)
-
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
+        glClearColor(1, 0, 0, 1)
 
     def OnDraw(self, *args, **kwargs):
         "Draw the window."
@@ -117,70 +129,27 @@ class CubeCanvas(MyCanvasBase):
     def InitGL(self):
         # set viewing projection
         glMatrixMode(GL_PROJECTION)
-        glOrtho(-1.0, 1.0, -1.0, 1.0, 1.0, 3.0)
-
-        # position viewer
-        glMatrixMode(GL_MODELVIEW)
-        glTranslatef(0.0, 0.0, -2.0)
-
-        # position object
-        glRotatef(self.y, 1.0, 0.0, 0.0)
-        glRotatef(self.x, 0.0, 1.0, 0.0)
+        (w, h) = self.GetGLExtents()
+        aspect = (float)(w) / (float)(h)
+        gluOrtho2D(-1.0, 1.0, -aspect, aspect)
 
         glEnable(GL_DEPTH_TEST)
-        #glEnable(GL_LIGHTING)
-        #glEnable(GL_LIGHT0)
 
     def OnDraw(self):
         # clear color and depth buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        # draw six faces of a cube
+        # draw bottom graph area
+        #glColor3f(self.r, self.g, self.b)
+        self.drawColor()
+        print "Drawing color"
         glBegin(GL_QUADS)
-        glNormal3f( 0.0, 0.0, 1.0)
-        glVertex3f( 0.5, 0.5, 0.5)
-        glVertex3f(-0.5, 0.5, 0.5)
-        glVertex3f(-0.5,-0.5, 0.5)
-        glVertex3f( 0.5,-0.5, 0.5)
-
-        glNormal3f( 0.0, 0.0,-1.0)
-        glVertex3f(-0.5,-0.5,-0.5)
-        glVertex3f(-0.5, 0.5,-0.5)
-        glVertex3f( 0.5, 0.5,-0.5)
-        glVertex3f( 0.5,-0.5,-0.5)
-
-        glNormal3f( 0.0, 1.0, 0.0)
-        glVertex3f( 0.5, 0.5, 0.5)
-        glVertex3f( 0.5, 0.5,-0.5)
-        glVertex3f(-0.5, 0.5,-0.5)
-        glVertex3f(-0.5, 0.5, 0.5)
-
-        glNormal3f( 0.0,-1.0, 0.0)
-        glVertex3f(-0.5,-0.5,-0.5)
-        glVertex3f( 0.5,-0.5,-0.5)
-        glVertex3f( 0.5,-0.5, 0.5)
-        glVertex3f(-0.5,-0.5, 0.5)
-
-        glNormal3f( 1.0, 0.0, 0.0)
-        glVertex3f( 0.5, 0.5, 0.5)
-        glVertex3f( 0.5,-0.5, 0.5)
-        glVertex3f( 0.5,-0.5,-0.5)
-        glVertex3f( 0.5, 0.5,-0.5)
-
-        glNormal3f(-1.0, 0.0, 0.0)
-        glVertex3f(-0.5,-0.5,-0.5)
-        glVertex3f(-0.5,-0.5, 0.5)
-        glVertex3f(-0.5, 0.5, 0.5)
-        glVertex3f(-0.5, 0.5,-0.5)
+        glVertex2f(-1.0, -1.0)
+        glVertex2f(-1.0, 0.0)
+        glVertex2f(1.0, 0.0)
+        glVertex2f(1.0, -1.0)
         glEnd()
-
-        w = h = min(self.GetGLExtents())
-        w = max(w, 1.0)
-        h = max(h, 1.0)
-        xScale = 180.0 / w
-        yScale = 180.0 / h
-        glRotatef((self.y - self.lasty) * yScale, 1.0, 0.0, 0.0);
-        glRotatef((self.x - self.lastx) * xScale, 0.0, 1.0, 0.0);
+        glColor3f(1.0, 1.0, 1.0)
 
         self.SwapBuffers()
 
@@ -196,7 +165,7 @@ class PageTwo(wx.Panel):
 
 app = wx.App(0)
 
-frame = wx.Frame(None, -1, size=(600,600))
+frame = wx.Frame(None, -1, size=(600,600), style = wx.RESIZE_BORDER)
 
 # Here we create a panel and a notebook on the panel
 p = wx.Panel(frame)
@@ -207,7 +176,7 @@ visPage = wx.Panel(nb)
 canvas = CubeCanvas(visPage)
 visSizer = wx.BoxSizer()
 #visSizer.Add(canvas, 1, wx.SHAPED | wx.ALIGN_CENTER)
-visSizer.Add(canvas, 1, wx.EXPAND | wx.ALIGN_CENTER)
+visSizer.Add(canvas, 1, wx.SHAPED | wx.ALIGN_LEFT)
 visPage.SetSizer(visSizer)
 
 dataPage = PageTwo(nb)
