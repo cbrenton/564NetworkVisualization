ad4e5d5bf2bb7847d1952a51b53555c8f72700b6
Andrew Sinclair <asinclair4@gmail.com>
2012-03-16 14:48:59 -0700

    Added some graphing functions.

--- filter.py
+++ filter.py
@@ -1,3 +1,22 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Filter: 
+   g = nx.Graph()
 
-
+   def __init__(self, host="", port=6969):
+      self.host = host
+      self.port = port
+      
+   def addFlowToGraph(routerSrc, dst, nextHop):
+      if nextHop == "0.0.0.0":
+         flowGraph.add_edge(routerSrc, dst)
+      else:
+         flowGraph.add_edge(routerSrc, nextHop)
+      
+   def updateNetworkGraph():
+      pos=nx.spring_layout(flowGraph)
+      nx.draw(flowGraph, pos, fontsize=10)
+      plt.axis('off')
+      plt.savefig("graph.png")
+  
